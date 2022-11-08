dae725f5d66c4487aecdb8b34ebaec509da49e16
David Dyck <dyckd15@icloud.com>
2022-11-08 11:51:36 -0600

    minor changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div>
       <title>Money-Manager</title>
       <h1 className='title'>Money Managing System</h1>
-      <a href='/listing'><button className='treedots'>Listings</button></a>
+      <p>Listing</p>
     </div>
   )
 }
