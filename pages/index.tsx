68715501eb949781328928213587309e341e1b67
David Dyck <dyckd15@icloud.com>
2022-11-08 12:43:13 -0600

    added Navigation Bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,17 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from "next/link";
+import Nav from "./components/nav.js"
 
 export default function Home() {
   return (
     <div>
+      <Nav></Nav>
       <title>Money-Manager</title>
       <h1 className='title'>Money Managing System</h1>
-      <p>Listing</p>
+      <p className='treedots'>Listing</p>
+      <Link href='/listing' className='button__one'><button>Listings</button></Link>
     </div>
   )
 }
--- pages/listing/index.tsx
+++ pages/listing/index.tsx
@@ -1,10 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import Link from "next/link";
+import Nav from "../components/nav.js"
+
 
 export default function Home() {
   return (
     <div>
+        <Nav></Nav>
       <title>Money-Manager</title>
       <h1 className='title'>Listings</h1>
       <p className='treedots'>...</p>
