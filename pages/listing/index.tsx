146d7463b84c05f38752513087db670c71ee58b7
David Dyck <dyckd15@icloud.com>
2022-11-08 11:44:16 -0600

    add listing page

--- pages/listing/index.tsx
+++ pages/listing/index.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div>
       <title>Money-Manager</title>
-      <h1 className='title'>Money Managing System</h1>
-      <a href='/listing'><button className='treedots'>Listings</button></a>
+      <h1 className='title'>Listings</h1>
+      <p className='treedots'>...</p>
     </div>
   )
 }
