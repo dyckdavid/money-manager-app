07bf00bad31f059a1ad76db49976c6e923850a9b
David Dyck <dyckd15@icloud.com>
2022-11-09 10:13:47 -0600

    Deleted Navbar starting from scratch using mantine

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App(props: AppProps) {
   return (
     <>
       <Head>
-        <title>Page title</title>
+        <title>Money Manager</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
 
@@ -17,7 +17,7 @@ export default function App(props: AppProps) {
         withNormalizeCSS
         theme={{
           /** Put your mantine theme override here */
-          colorScheme: 'light',
+          colorScheme: 'dark',
         }}
       >
         <Component {...pageProps} />
