3502f3dea972b9de0c2a1eb1669b8a9536e80fec
David Dyck <dyckd15@icloud.com>
2022-11-15 17:01:38 -0600

    update

--- pages/admin/add/formone.tsx
+++ pages/admin/add/formone.tsx
@@ -1,44 +1,16 @@
 import { TextInput, MultiSelect, NumberInput, MantineProvider } from '@mantine/core';
 
 function Demo() {
-  return (
-    <MantineProvider
-      theme={{
-        components: {
-          InputWrapper: {
-            defaultProps: {
-              inputWrapperOrder: ['label', 'error', 'input', 'description'],
-            },
-          },
-
-          Input: {
-            defaultProps: {
-              variant: 'filled',
-            },
-          },
-        },
-      }}
-    >
-      <TextInput
-        label="Name"
-        placeholder="Name"
-        description="Add your Name"
-      />
-      <NumberInput
-        mt="xl"
-        label="Age"
-        placeholder="Age"
-        description="Add your Age"
-      />
-      <MultiSelect
-        mt="xl"
-        data={['Apple', 'Android']}
-        label="Select "
-        placeholder="Select"
-        description="Select"
-      />
-    </MantineProvider>
-  );
-}
+    return (
+      <><TextInput
+            placeholder="Your name"
+            label="Full name"
+            withAsterisk /><TextInput
+                placeholder="Income"
+                label="Full Income"
+                withAsterisk /></>
+      
+    );
+  }
 
 export default Demo;
