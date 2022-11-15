13954c8620ae8d35d53eda4ae5eab88883fd9f5f
David Dyck <dyckd15@icloud.com>
2022-11-15 16:06:03 -0600

    centered Title and Button on home page!

--- pages/components/button.tsx
+++ pages/components/button.tsx
@@ -1,13 +1,16 @@
 import { Button } from '@mantine/core';
 import Link from 'next/link'
+import { Center } from '@mantine/core';
 
 function Demo() {
   return (
+    <Center>
     <Link href="/listing">
     <Button radius="md" size="md">
       Listing
     </Button>
     </Link>
+    </Center>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css'
 import Link from "next/link";
 import { ServerStyles, createStylesServer } from '@mantine/next';
 import Button from './components/button'
+import { Center } from '@mantine/core';
 
 
 
@@ -12,7 +13,9 @@ export default function Home() {
     <div>
       
       <title>Money-Manager</title>
+      <Center>
       <h1 className='title'>Money Managing System</h1>
+      </Center>
       
       <Button></Button>
     </div>
