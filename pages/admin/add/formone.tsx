2a317bf7fedd89eebfd26916d10dafc19145739f
David Dyck <dyckd15@icloud.com>
2022-11-16 12:00:50 -0600

    minor changes to admin/add page

--- pages/admin/add/formone.tsx
+++ pages/admin/add/formone.tsx
@@ -1,14 +1,22 @@
 import { TextInput, MultiSelect, NumberInput, MantineProvider } from '@mantine/core';
+import { Space } from '@mantine/core';
+import Submit from './submit'
 
 function Demo() {
     return (
       <><TextInput
-            placeholder="Your name"
-            label="Full name"
-            withAsterisk /><TextInput
+      style={{ width: 400, marginLeft: 'auto', marginRight: 'auto' }}
+            placeholder="Date"
+            label="Enter Date"
+            withAsterisk /><Space h="xl" /><TextInput
+            style={{ width: 400, marginLeft: 'auto', marginRight: 'auto' }}
                 placeholder="Income"
                 label="Full Income"
-                withAsterisk /></>
+                withAsterisk /><Space h="xl" /><TextInput
+                style={{ width: 400, marginLeft: 'auto', marginRight: 'auto' }}
+                placeholder="Expense"
+                label="Full Expense"
+                withAsterisk /><Space h="xl" /><Submit></Submit></>
       
     );
   }
---
+++ pages/admin/add/submit.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@mantine/core';
+import Link from 'next/link'
+import { Center } from '@mantine/core';
+
+function Demo() {
+  return (
+    <Center>
+    <Link href="../admin/add">
+    <Button radius="lg" size="xl">
+      Submit
+    </Button>
+    </Link>
+    </Center>
+  );
+}
+
+export default Demo;
