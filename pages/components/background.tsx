432513b925beefc0c10413aa42d78d1e3487dac8
David Dyck <dyckd15@icloud.com>
2022-11-16 11:53:59 -0600

    added responsiv image to home page

--- pages/components/background.tsx
+++ pages/components/background.tsx
@@ -1,8 +1,30 @@
 import { Image } from '@mantine/core';
+import { createStyles } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  container: {
+    height: 100,
+    width: 350,
+
+    // Media query with value from theme
+    [`@media (max-width: ${theme.breakpoints.xl}px)`]: {
+        width: 1000,
+      
+    },
+
+    // Static media query
+    '@media (max-width: 800px)': {
+      width: 300,
+    },
+  },
+}));
+
+
 
 function Imagedemo() {
+    const { classes } = useStyles();
   return (
-    <div style={{ width: 650, marginLeft: 'auto', marginRight: 'auto' }}>
+    <div style={{ marginLeft: 'auto', marginRight: 'auto' }} className={classes.container}>
       <Image
         radius="xl"
         src="https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_960_720.jpg"
