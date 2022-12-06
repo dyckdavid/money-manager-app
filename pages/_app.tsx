a818cff635f2851e14377d67aa760c38973d02f5
David Dyck <dyckd15@icloud.com>
2022-12-06 14:55:36 -0600

    Deleted current Navbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { MantineProvider } from '@mantine/core';
-import Navbar from '../pages/components/Navbars'
 import "../styles/globals.css";
 
 
@@ -15,7 +14,7 @@ export default function App(props: AppProps) {
         <title>Money Manager</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
-      <Navbar />
+      
 
       <MantineProvider
         withGlobalStyles
--- pages/components/Logo.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Logo = () => {
-  return <div className="logo">Money-Manager</div>;
-};
-
-export default Logo;
