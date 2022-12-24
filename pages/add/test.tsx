0dd2b55c7a0f3c79b20c30e4ce4a461d2dbef8a9
David Dyck <dyckd15@icloud.com>
2022-12-23 23:45:15 -0600

    another feature im working on

--- pages/add/test.tsx
+++ pages/add/test.tsx
@@ -1,145 +1,57 @@
-import {
-    TextInput,
-    NumberInput,
-    Button,
-    Flex,
-  } from "@mantine/core";
-  import { Space } from "@mantine/core";
-  import { DatePicker } from "@mantine/dates";
-  import {
-    collection,
-    addDoc,
-    serverTimestamp,
-    DocumentData,
-    getDocs,
-  } from "firebase/firestore";
-  import { db } from "../../firebase/clientApp";
-  import { useForm } from "@mantine/form";
-  import { Loader } from '@mantine/core';
-import { useState } from "react";
-import { Center } from "@mantine/core";
-import Listingback from "../components/listingback";
-import React from 'react';
-import { ActionIcon } from '@mantine/core';
-import { IconSettings } from '@tabler/icons';
-import Nav from '../components/navbar';
-import Link from 'next/link';
-import { Select } from '@mantine/core';
-import { useEffect } from 'react';
+import React, { useState } from 'react';
 
+function MyForm() {
+  // Declare a state variable for the selected option
+  const [selectedOption, setSelectedOption] = useState('');
 
+  // Declare a state variable for the income text field
+  const [income, setIncome] = useState('');
 
+  // Declare a state variable for the expense text field
+  const [expense, setExpense] = useState('');
 
+  // Function to handle changes to the select element
+  function handleOptionChange(event: { target: { value: React.SetStateAction<string>; }; }) {
+    setSelectedOption(event.target.value);
+  }
 
-const AddListing = () => {
-    const [Loading, setLoading] = useState(false)
-    const [Disabled, setDisable] = useState(false)
+  // Function to handle changes to the income text field
+  function handleIncomeChange(event: { target: { value: React.SetStateAction<string>; }; }) {
+    setIncome(event.target.value);
+  }
 
-
-    
-    const [add, setadd] = useState({name: '', description: '', income: '', expense: ''})
-    const onSubmit = async () => {
-        setLoading(true)
-        setDisable(true)
-        const collectionRef = collection(db, "manager");
-        
-        const docRef = await addDoc(collectionRef, { ...add, timestamp: serverTimestamp() })
-            setadd({ name: '', description: '', income: '', expense: ''})
-            
-            setLoading(false)
-            setDisable(false)
-            
-    }
-
-
-
-
-function SubmitButton(){
-    if (add.name && add.description && add.income && add.expense){
-      return <Button  loading={Loading} onClick={onSubmit} sx={{ mt: 3 }}>Submit</Button>
-    } else {
-      return <Button loading={Loading} onClick={onSubmit} sx={{ mt: 3 }} disabled >Submit</Button>
-    };
-};
-
-
-
-
-function IsorNot() {
-
+  // Function to handle changes to the expense text field
+  function handleExpenseChange(event: { target: { value: React.SetStateAction<string>; }; }) {
+    setExpense(event.target.value);
+  }
 
   return (
-    <h1>hello</h1>
-  );
-
-    
-}
-
-function HideOrNot() {
-  
-  return (
-    <>
-    <Select
-      label="Your favorite framework/library"
-      placeholder="Pick one"
-      data={[
-        { value: 'income', label: 'React' },
-        { value: 'expense', label: 'Angular' },
-      ]}
-    />
-    <Button id="income" className="income">Income</Button>
-    <Button id="expense" className="expense">Expense</Button>
-    </>
-
-  );
-}
-
-
-
-
-
-
-
-
-    return (
-
-        
+    <form>
+      <label>
+        Select an option:
+        <select value={selectedOption} onChange={handleOptionChange}>
+          <option value="income">Income</option>
+          <option value="expense">Expense</option>
+        </select>
+      </label>
+      {selectedOption === 'income' && (
         <div>
-            <Nav></Nav>
-      <Space h="xl" />
-      
-        <Space w="xs" /><Center><Link href="/add/settings"><ActionIcon variant="filled"><IconSettings size={36} /></ActionIcon></Link></Center>
-
-        <Space w="md" />
-        
-      
-      <Center><h1 className='title'>ADD LISTING</h1></Center>
-            
-            <TextInput label="Name"
-            style={{ width: 400, marginLeft: "auto", marginRight: "auto" }}
-             value={add.name}
-             onChange={e => setadd({...add, name: e.target.value})}
-             />
-            <TextInput label="Description"
-            style={{ width: 400, marginLeft: "auto", marginRight: "auto" }}
-             value={add.description} 
-             onChange={e => setadd({...add, description: e.target.value})}
-             />
-            <IsorNot></IsorNot>
-             <TextInput label="Income"
-            style={{ width: 400, marginLeft: "auto", marginRight: "auto" }}
-             value={add.income} 
-             onChange={e => setadd({...add, income: e.target.value})}
-             />
-             <TextInput label="Expense"
-            style={{ width: 400, marginLeft: "auto", marginRight: "auto" }}
-             value={add.expense} 
-             onChange={e => setadd({...add, expense: e.target.value})}
-             />
-             <Space h={10}/>
-            <Center><SubmitButton></SubmitButton></Center>
+          <label>
+            Income:
+            <input type="text" value={income} onChange={handleIncomeChange} />
+          </label>
         </div>
-    )
+      )}
+      {selectedOption === 'expense' && (
+        <div>
+          <label>
+            Expense:
+            <input type="text" value={expense} onChange={handleExpenseChange} />
+          </label>
+        </div>
+      )}
+    </form>
+  );
 }
 
-export default AddListing;
+export default MyForm;
